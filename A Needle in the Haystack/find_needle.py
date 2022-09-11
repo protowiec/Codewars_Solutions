d44f37673cc97bb57acf6514ce3f1ac78ba195f5
protowiec <protowiecgithub@gmail.com>
2022-09-11 18:06:52 +0200

    Find a needle

--- A Needle in the Haystack/find_needle.py
+++ A Needle in the Haystack/find_needle.py
@@ -8,7 +8,6 @@ if __name__ == "__main__":
 
 
 """
-
 Write a function findNeedle() that takes an array full of junk but containing one "needle"
 
 After your function finds the needle it should return a message (as a string) that says:
